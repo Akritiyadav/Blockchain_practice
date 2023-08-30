f4a8823f77760c8ebcff0bb34c6a38f5a22585e6
Akritiyadav <akritiyadav895@gmail.com>
2023-08-30 18:53:35 +0530

    blockchain basic done

---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+require('dotenv').config({path:'./.env.local'});
+require("@nomiclabs/hardhat-waffle");
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+ 
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+})
+
+const privatekey = process.env.NEXT_PUBLIC_PRIVATE_KEY
+module.exports = {
+  solidity: "0.8.10",
+  defaultNetwork:"polygon",
+  networks:{
+    hardhat:{},
+    polygon:{
+   url:process.env.NEXT_PUBLIC_RPC_URL,
+   accounts:[privatekey]
+    }
+  }
+};
